21e33201bf81790b818a6ae4113de2b6532cb455
Spockuto <venkythesupersaiyan@gmail.com>
2018-07-26 14:51:05 +0530

    Canvas Centering. Four Fractals have been added with their default iteration system provided. More generic system need to be implemented.

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,4 @@
+//Fractal Tree
 var lObject1 = new Object();
 lObject1["variables"] = "XF";
 lObject1["constants"] = "+−[]";
@@ -8,19 +9,25 @@ lObject1["axiom"] = "X";
 lObject1["rule1"] = "X=F+[[X]-X]-F[-FX]+X";
 lObject1["rule2"] = "F=FF";
 lObject1["rule3"] = "";
+lObject1["length"] = 0;
+lObject1["canvasHeight"] = 0;
 
+//Dragon Curve
 var lObject2 = new Object();
 lObject2["variables"] = "XY";
 lObject2["constants"] = "+−[]";
 lObject2["angle"] = 90;
-lObject2["round"] = 14;
+lObject2["round"] = 12;
 lObject2["axiom"] = "FX";
 //Shoud be of the form X = XY;
 lObject2["rule1"] = "X=X+YF+";
 lObject2["rule2"] = "Y=-FX-Y";
 lObject2["rule3"] = "";
+lObject2["length"] = 20;
+lObject2["canvasHeight"] = window.innerHeight/2 - 200;
 
 
+//Frec Fractal
 var lObject3 = new Object();
 lObject3["variables"] = "XY";
 lObject3["constants"] = "+−[]";
@@ -31,7 +38,10 @@ lObject3["axiom"] = "XYXYXYX+XYXYXYX+XYXYXYX+XYXYXYX";
 lObject3["rule1"] = "F=";
 lObject3["rule2"] = "X=FX+FX+FXFY-FY-";
 lObject3["rule3"] = "Y=+FX+FXFY-FY-FY";
+lObject3["length"] = -10;
+lObject3["canvasHeight"] = window.innerHeight - 200;
 
+//Bend Tree
 var lObject4 = new Object();
 lObject4["variables"] = "F";
 lObject4["constants"] = "+−[]";
@@ -42,34 +52,32 @@ lObject4["axiom"] = "F";
 lObject4["rule1"] = "F=FF-[-F+F+F]+[+F-F-F]";
 lObject4["rule2"] = "";
 lObject4["rule3"] = "";
+lObject4["length"] = 0;
+lObject4["canvasHeight"] = 0;
 
+
+//Initial Object
 var lObject = lObject1;
+
+
 var ruleDict = new Object();
 var stack = [];
-var stackObject = new Object();
-stackObject["x"] = "";
-stackObject["y"] = "";
-stackObject["angle"] = "";
-var canvasHeight;
+
 function selectFractal(i){
     if(i == 1){
         lObject = lObject1;
-        canvasHeight = 0;
     }
 
     if(i == 2){
         lObject = lObject2;
-        canvasHeight = 200;
     }
 
     if(i == 3){
-        lObject  = lObject3;  
-        canvasHeight = window.innerHeight/2;  
+        lObject  = lObject3;   
     }
 
     if(i == 4){
         lObject = lObject4;
-        canvasHeight = 0;
     }
 }
 
@@ -80,9 +88,10 @@ function generateRuleDict(){
 }
 
 function generateSequence(){
-    lObject.round = parseInt(document.getElementById('iterator').value);
-    if(lObject.round == null)
-        lObject.round = 5;
+
+    if(document.getElementById('iterator').value != "")
+        lObject.round = parseInt(document.getElementById('iterator').value); 
+
     var round = lObject.round;
     var sequence;
     var variables = lObject.variables.split("");
@@ -110,15 +119,21 @@ function generateSequence(){
 }
 
 function drawStuff() {
+        context.translate(canvas.width/2, canvas.height - lObject.canvasHeight);
         context.moveTo(0,0);
-        var r = 30/lObject.round;
+        if(document.getElementById('iterator').value != "")
+            lObject.round = parseInt(document.getElementById('iterator').value); 
+        var r = (lObject.length + 30 ) / lObject.round;
         var lastPosX = 0;
         var lastPosY = 0;
         var lastangle = lObject.angle;
         var angleplus = Math.PI * lObject["angle"] / 180.0;
         var angleminus = -1 * Math.PI * lObject["angle"] / 180.0;
         var sequence = generateSequence();
+       // console.log(sequence);
+        console.log(r);
         for(var i = 0 ; i < sequence.length; i++){
+           // console.log(i);
             if(sequence[i] == 'F'){
                 lastPosX = lastPosX + r * Math.cos(lastangle);
                 lastPosY = lastPosY + r * Math.sin(lastangle);
@@ -160,7 +175,6 @@ function drawStuff() {
     function resizeCanvas() {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight - 100;
-            context.translate(canvas.width/2,canvas.height - canvasHeight);
             drawStuff(); 
     }
     resizeCanvas();
